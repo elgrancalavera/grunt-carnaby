cb9f746e3d744ca96a293133b05e355be09a9232
Leon Coto <leon.coto@mcsaatchi.com>
2013-08-14 15:22:28 +0100

    Updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,18 +60,13 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('c', ['c:template']);
-
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'c', 'nodeunit']);
 
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('code', ['default', 'watch']);
 
 };
